eebfedb11b60dade1b39748e23bdc78ea0b1d44f
louis baptiste <louisvis@ymail.com>
2020-03-12 20:17:38 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,8 +45,8 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
   def upload
-    CSV.foreach(params[:songs].path, headers: true) do |lead|
-      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+    CSV.foreach(params[:songs].path, headers: true) do |row|
+      Song.create(title: row[0])
     end
     redirect_to customers_path
   end
